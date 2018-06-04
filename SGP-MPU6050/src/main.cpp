25cf4e5d402a26794ff66e77db8fc24bd510b188
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-06-04 20:54:38 +0700

    add

--- SGP-MPU6050/src/main.cpp
+++ SGP-MPU6050/src/main.cpp
@@ -11,9 +11,11 @@ float gForceX, gForceY, gForceZ, rotX, rotY, rotZ;
 float Acc[2];
 float Gy[3];
 float Angle[3];
-String value;
+
 long time_prev;
 float dt;
+
+String value;
 
 void dataReceiver();
 void processData();
@@ -39,17 +41,6 @@ void loop()
 //###################################################################################
 void dataReceiver()
 {
-    // Wire.beginTransmission(MPU);
-    // Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
-    // Wire.endTransmission(false);
-    // Wire.requestFrom(MPU, 14, true);      // request a total of 14 registers
-    // AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-    // AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    // AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    // GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    // GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    // GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
     Wire.beginTransmission(MPU);
     Wire.write(0x3B);
     Wire.endTransmission(false);
